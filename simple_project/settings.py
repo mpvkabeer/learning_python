323d9cd7f20fa7028c9091f2b8aa7c98fb99feff
Ahamed Kabeer <126021136+mpvkabeer@users.noreply.github.com>
2026-02-13 00:25:25 +0530

    Update settings.py

--- simple_project/settings.py
+++ simple_project/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'simple_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'simple_db',
+        'USER': 'root',
+        'PASSWORD': 'kabeer',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
 
